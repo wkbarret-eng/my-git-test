9002c812d607f059e1fa7062137264442fada69d
yillkid <yillkid@gmail.com>
2024-03-14 10:08:09 +0000

    fix: revise answer

--- docker-compose-command/workshop/flask/frontend/server.py
+++ docker-compose-command/workshop/flask/frontend/server.py
@@ -1,19 +1,29 @@
+import os
 import requests
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
+backend_host = "http://192.168.0.100:5002"
+
 @app.route("/signup", methods = ["GET"])
 def signup():
-    with open("/app/logs/log.txt","w") as f:
+    log_directory = "/app/logs"
+    log_file_path = os.path.join(log_directory, "log.txt")
+
+    # 檢查目錄是否存在，如果不存在，則創建它
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+
+    with open(log_file_path, "w") as f:
         f.write("(Frontend) Got signup request !")
 
-    return render_template("signup.html")
+    return render_template("signup.html", backend_ip = backend_host)
 
 @app.route("/listAccounts", methods = ["GET"])
 def listAccount():
-    response = requests.get("http://backend_host:5001/listAccounts")
+    response = requests.get(backend_host + "/listAccounts")
 
     return render_template("listAccount.html", user = response.text)
 
